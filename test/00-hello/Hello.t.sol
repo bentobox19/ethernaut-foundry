742f614a701640624b0a9ce087f3114564f8cbaa
bentobox19.eth <bentobox19@protonmail.com>
2023-02-17 23:32:31 -0300

    Add an alternative solution to 00-hello

--- test/00-hello/Hello.t.sol
+++ test/00-hello/Hello.t.sol
@@ -23,4 +23,26 @@ contract HelloTest is Test {
 
     utils.submitLevelInstance(challengeAddress);
   }
+
+  // alternate solution: you can't read the factory.
+  // you can always read the blockchain though
+  function _testExploit() public {
+
+    // get the contents of the slot 0 from the challenge address,
+    // assuming that this is a string with less than 32 bytes,
+    // in that case, length the lowest-order byte stores the value length * 2.
+    bytes32 slot0 = vm.load(challengeAddress, bytes32(uint256(0)));
+    uint8 len = uint8(slot0[31]) / 2;
+
+    bytes memory password = new bytes(len);
+
+    for (uint8 i = 0; i < len; i++) {
+      password[i] = slot0[i];
+    }
+
+    // test the found password in the contract
+    challenge.authenticate(string(password));
+
+    utils.submitLevelInstance(challengeAddress);
+  }
 }
