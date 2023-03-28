168fbfefa7f824d5453fd91d1ca86587ec3bfeb9
bentobox19.eth <bentobox19@protonmail.com>
2023-03-28 02:32:45 -0300

    WIP 27-good-samaritan

--- test/27-good-samaritan/GoodSamaritan.t.sol
+++ test/27-good-samaritan/GoodSamaritan.t.sol
@@ -1,5 +1,17 @@
-//    "27": "0x8d07AC34D8f73e2892496c15223297e5B22B3ABE",
-//    "28": "0x762db91C67F7394606C8A636B5A55dbA411347c6",
-
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
+
+import "forge-std/Test.sol";
+import "../utils.sol";
+
+contract GoodSamaritanTest is Test {
+  address internal challengeAddress;
+
+  function setUp() public {
+    challengeAddress = utils.createLevelInstance(0x8d07AC34D8f73e2892496c15223297e5B22B3ABE);
+  }
+
+  function testExploit() public {
+    // utils.submitLevelInstance(challengeAddress);
+  }
+}
---
+++ test/28-gatekeeper-three/GatekepperThree.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+// import "forge-std/Test.sol";
+// import "../utils.sol";
+
+// contract GatekeeperThreeTest is Test {
+//   address internal challengeAddress;
+
+//   function setUp() public {
+//     challengeAddress = utils.createLevelInstance(0x762db91C67F7394606C8A636B5A55dbA411347c6);
+//   }
+
+//   function testExploit() public {
+//     // utils.submitLevelInstance(challengeAddress);
+//   }
+// }
